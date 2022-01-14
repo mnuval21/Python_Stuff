457659bd820e903dbcef3a554afdb5a76b6f1c66
Melissa Nuval <mnuval21@gmail.com>
2022-01-14 01:39:43 -0500

    first commit - loops

---
+++ loops.py
@@ -0,0 +1,51 @@
+#num = 0
+#while num <= 100:
+#    print(num)
+#    if num == 50:
+#        break
+#    print(num)
+#    num = num + 10 
+
+#print("ALL DONE counting!")
+
+#target = 37
+#guess = None
+
+#while guess != target:
+#    guess = input("Please enter a guess:   ")
+#    if guess == 'q' or guess == 'quit':
+#        break
+#    guess = int(guess)
+#print("All DONE")
+
+colors = ['red', 'orange', 'yellow', 'green']
+for color in colors:
+    print(color)
+
+for char in  "ABCDEFGHIJKLMNOP":
+    print(char)
+
+for num in "abcde":
+    print("Hello")
+
+for n in range(10):
+    print(n)
+
+range(9)
+range(0, 9)
+
+for num in range(9):
+    print(num)
+
+list(range(10))
+
+list(range(5,10))
+
+list(range(-100,0))
+
+list(range(1,10,2))
+
+list(range(2,12,2))
+
+#go backwards
+list(range(10,0,-1))
